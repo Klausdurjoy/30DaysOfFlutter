17469eec1bc509b8d5a8b51823979e63447c67b8
HP <durjoybarua31@gmail.com>
2021-11-04 03:25:59 +0600

    Day 2 completed

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,25 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+class LoginPage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child:Text("Login Page",style: TextStyle(
+          fontSize: 20,
+          color: Colors.lightBlueAccent,
+          fontWeight: FontWeight.bold,
+
+        ),
+        textScaleFactor: 1.0,
+        ),
+
+      ),
+
+
+    );
+  }
+}
